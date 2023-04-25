e61b55de0fba0d96a735f5a3af1c2dce779ed27d
hendersonh <hendy@hendersonhood.com>
2023-04-25 13:10:33 -0400

    added T.cloud as backend for state

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -1,29 +1,13 @@
-terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
 module "webserver_cluster" {
-
-  # Since the terraform-up-and-running-code repo is open source, we're using an HTTPS URL here. If it was a private
-  # repo, we'd instead use an SSH URL (git@github.com:brikis98/terraform-up-and-running-code.git) to leverage SSH auth
-  source = "github.com/brikis98/terraform-up-and-running-code//code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster?ref=v0.3.0"
+  
+  //source = "github.com/brikis98/terraform-up-and-running-code//code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster?ref=v0.3.0"
+  source = "github.com/hendersonh/modules/services/webserver-cluster"
 
   cluster_name           = var.cluster_name
-  db_remote_state_bucket = var.db_remote_state_bucket
-  db_remote_state_key    = var.db_remote_state_key
+  oraganization = var.oraganization
+  workspace     = var.workspace
 
-  instance_type = "m4.large"
+  instance_type = "db.t2.micro"
   min_size      = 2
   max_size      = 10
 }
--- prod/services/webserver-cluster/variables.tf
+++ prod/services/webserver-cluster/variables.tf
@@ -3,14 +3,16 @@
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket used for the database's remote state storage"
+variable "organization" {
+  description = "Organization name"
   type        = string
+  default = "hendy"
 }
 
-variable "db_remote_state_key" {
-  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+variable "workspace" {
+  description = "Work space in cloud"
   type        = string
+  default     = "live"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
